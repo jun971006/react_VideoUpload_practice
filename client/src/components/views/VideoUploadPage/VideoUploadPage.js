113668c40399ba9d0c5316085b30e2f51c308d4d
Lee junseok <jun971006@gmail.com>
2020-11-24 22:40:16 +0900

    first Commit!

---
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -0,0 +1,169 @@
+import React, { useState } from 'react'
+import { Typography, Button, Form, message, Input, Icon } from 'antd'
+import Dropzone from 'react-dropzone'
+
+import Axios from 'axios'
+
+const { TextArea } = Input;
+const { Title } = Typography;
+
+const PrivateOptions = [
+    {value:0, label: "Priavate"},
+    {value:1, label: "Public"}
+]
+
+const CategoryOptions = [
+    {value:0, label: "Film & Animation"},
+    {value:1, label: "Autos & Vehicle"},
+    {value:2, label: "Music"},
+    {value:3, label: "Pets & Animals"}, 
+]
+
+
+export default function Videouploadpage(props) {
+    
+    const [VideoTitle, setVideoTitle] = useState("")
+    const [Description, setDescription] = useState("")
+    const [Private, setPrivate] = useState(0)
+    const [Category, setCategory] = useState("Film & Animation")
+    const [FilePath, setFilePath] = useState("")
+    const [Duration, setDuration] = useState("")
+    const [ThumbnailPath, setThumbnailPath] = useState("")
+    
+    const onTitleChange = (e) => {
+        //console.log(e.currentTarget.value)
+        setVideoTitle(e.currentTarget.value)
+    }
+
+    const onDescriptionChange = (e) => {
+        setDescription(e.currentTarget.value)
+    }
+
+    const onPrivateChange = (e) => {
+        setPrivate(e.currentTarget.value)
+    }
+
+    const onCategoryChange = (e) => {
+        setCategory(e.currentTarget.value)
+    }
+
+    const OnDrop = (files) => {
+        let formData = new FormData;
+        const config = {
+            header : {'content-type': 'multipart/form-data'}
+        }
+        formData.append("file", files[0])
+
+        console.log("1"+files[0])
+
+        Axios.post('/api/video/uploadfiles', formData, config)
+            .then(response => {
+                if(response.data.success) {
+                    console.log(response.data)
+
+                    let variable = {
+                        url: response.data.url,
+                        fileName: response.data.fileName 
+                    }
+
+                    setFilePath(response.data.url)
+
+                    Axios.post('/api/video/thumbnail', variable)
+                        .then(response => {
+                            if(response.data.success ){
+                                console.log(response.data)
+
+                                // 썸네일 생성 후 State로 저장해야함
+
+                                setDuration(response.data.fileDuration)
+                                setThumbnailPath(response.data.url)
+                            }
+                            else{
+                                alert('썸네일 생성에 실패했습니다.')
+                            }
+                        })
+                } else{
+                    console.log(response.data.success)
+                    alert('비디오 업로드 실패')
+                }
+            })
+    }
+
+
+
+    return (
+        <div style={{maxWidth:'700px', margin:'2rem auto'}}>
+            <div style={{textAlign:'center', marginBottom:'2ren'}}>
+                <Title level={2}>Upload Video</Title>
+            </div>
+
+            <Form onSubmit>
+                <div style={{display:'flex', justifyContent:'content-between'}}>
+                    {/* Drop Zone */}
+                    <Dropzone
+                    onDrop={OnDrop}
+                    multiple={false}
+                    maxSize={1000000000}
+                    >
+                    {({getRootProps, getInputProps})=>(
+                        <div style={{width:'300px', height:'240px', border:'1px solid lightgray', display:'flex', alignItems:'center', justifyContent:'center'}}{...getRootProps()}>
+                            <input {...getInputProps()}/>
+                            <Icon type="plus" style={{fontSize:'3rem'}} />
+
+                        </div>
+                    )}
+
+                    </Dropzone>
+
+                    {/* Thumbnail Zone */}
+                    {ThumbnailPath &&
+                        <div>
+                            <img src={`http://localhost:5000/${ThumbnailPath}`} alt="thumbnail"/>
+                        </div>
+                    }
+                    
+                    
+                </div>
+
+                <br />
+                <br />
+                <label>Title</label>
+                <Input 
+                    onChange={onTitleChange}
+                    value={VideoTitle}
+                />
+                <br />
+                <br />
+                <label>Description</label>
+                <TextArea
+                    onChange={onDescriptionChange}
+                    value={Description}
+                />
+
+                <br />
+                <br />
+                    <select onChange={onPrivateChange}>
+
+                        {PrivateOptions.map((item, index) => (
+                            <option key={index} value={item.value}>{item.label}</option>
+                        ))}
+                        
+                    </select>
+                <br />
+                <br />
+                <select onChange={onCategoryChange}>
+                    {CategoryOptions.map((item, index)=>(
+                        <option key={index} value={item.value}>{item.label}</option>
+                    ))}
+                    
+                </select>
+                <br />
+                <br />
+                <Button type="primary" sife="large" onClick>
+                    Submit
+                </Button>
+            </Form>
+              
+        </div>
+    )
+}
---
+++ server/routes/video.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const router = express.Router();
+//const { Video } = require("../models/Video");
+const path = require('path')
+const { auth } = require("../middleware/auth");
+const multer = require('multer')
+const ffmpeg = require('fluent-ffmpeg')
+
+
+let storage = multer.diskStorage({
+    destination:(req, file, cb) => {
+        cb(null, "uploads/");
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}_${file.originalname}`);
+    },
+    fileFilter:(req, file, cb)=>{
+        const ext = path.extname(file.originalname)
+        if(ext !== '.mp4'){
+            return cb(res.status(400).end('only mp4 is allowed'), false)
+        }
+        cb(null, true)
+    }
+});
+
+const upload = multer({storage: storage}).single("file");
+
+//=================================
+//             Video
+//=================================
+
+router.post('/uploadfiles', (req, res) => {
+
+    //console.log("2"+req.files)
+    // 클라이언트에서 받은 비디오 서버에 저장
+    upload(req, res, err => {
+        if(err){
+            return res.json({success : false, err})
+        }
+        return res.json({success:true, url: res.req.file.path, filename: res.req.file.filename})
+    })
+
+
+})
+
+router.post('/thumbnail', (req, res) => {
+    // 썸네일 생성 하고 비디오 러닝타임도 가져오기..(정보)
+
+    let filePath = ""
+    let fileDuration = ""
+
+    // 비디오 정보 가져오기
+    ffmpeg.ffprobe(req.body.url, function(err, metadata){
+        //console.dir(metadata)
+        //console.log(metadata.format.duration);
+        fileDuration = metadata.format.duration;
+    });
+
+    // 썸네일 생성
+    ffmpeg(req.body.url)
+    .on('filenames', function(filenames){
+        console.log('will generate ' + filenames.join(', '))
+        console.log(filenames)
+
+        filePath = "uploads/thumbnails/" + filenames[0]
+    })
+    .on('end', function(){
+        console.log('ScreenShots taken');
+        return res.json({success:true, url:filePath, fileDuration: fileDuration})
+    })
+    .on('error', function(err){
+        console.error(err)
+        return res.json({success:false, err})
+    })
+    .screenshot({
+        count:3,
+        folder: 'uploads/thumbnails',
+        size : '320x240',
+        filename: 'thumbnail-%b.png'
+    })
+})
+
+module.exports = router;
+
+/*
+router.post("/login", (req, res) => {
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user)
+            return res.json({
+                loginSuccess: false,
+                message: "Auth failed, email not found"
+            });
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch)
+                return res.json({ loginSuccess: false, message: "Wrong password" });
+
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+                res.cookie("w_authExp", user.tokenExp);
+                res
+                    .cookie("w_auth", user.token)
+                    .status(200)
+                    .json({
+                        loginSuccess: true, userId: user._id
+                    });
+            });
+        });
+    });
+});
+*/
